fd522c29cbe748848e4944439eb0212ad144bbc0
kayi920312 <468862162@qq.com>
2017-06-16 22:47:35 +0800

    一些配置修改

--- Web12306/src/main/java/util/HttpUtils.java
+++ Web12306/src/main/java/util/HttpUtils.java
@@ -159,5 +159,63 @@ public class HttpUtils {
             }
         }
         return result;
+    }  
+	
+	
+	
+	
+	
+	public static String sendPost2(String url, String param) {
+    	//POST请求 需使用OutputStreamWriter输出流输出参数
+    	OutputStreamWriter out = null;
+        BufferedReader in = null;
+        String result = "";
+        try {
+            URL realUrl = new URL(url);
+            // 打开和URL之间的连接
+            URLConnection conn = realUrl.openConnection();
+            // 设置通用的请求属性
+            conn.setRequestProperty("accept", "*/*");
+            conn.setRequestProperty("connection", "Keep-Alive");
+            conn.setRequestProperty("user-agent","Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1;SV1)");
+            conn.setRequestProperty("Content-Type","application/x-www-form-urlencoded");
+            // 发送POST请求必须设置如下两行
+            conn.setDoOutput(true);
+            conn.setDoInput(true);
+            // 获取URLConnection对象对应的输出流
+            out = new OutputStreamWriter(conn.getOutputStream(),"utf-8");
+            // 发送请求参数
+            out.write(param);
+            // flush输出流的缓冲
+            out.flush();
+            // 定义BufferedReader输入流来读取URL的响应
+            in = new BufferedReader(new InputStreamReader(conn.getInputStream(),"utf-8"));
+            String line;
+            while ((line = in.readLine()) != null) {
+                result += line;
+            }
+        } catch (Exception e) {
+        	logger.info("发送post请求： "+e.getMessage());
+        }
+        //使用finally块来关闭输出流、输入流
+        finally{
+            try{
+                if(out!=null){
+                    out.close();
+                }
+                if(in!=null){
+                    in.close();
+                }
+            }
+            catch(IOException ex){
+                ex.printStackTrace();
+            }
+        }
+        return result;
     }    
+	
+	public static void main(String[] args) {
+		String result = sendPost2("https://token.beyondverbal.com/token", "grant_type=client_credentials&apiKey=586886d0-02e3-4dbb-810e-f504ca08e1c5");
+		System.out.println(result);
+	}
 }
